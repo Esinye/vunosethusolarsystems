5cc5e640273d459c24cd99f413e9c32d5bccdd2b
Esinye <vunosethu@gmail.com>
2025-05-29 11:00:31 +0200

    Add files via upload
    Vunosethu Solar Systems

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contactBtn = document.querySelector('.contact-btn');
+    
+    contactBtn.addEventListener('click', () => {
+        alert('Thank you for your interest! Please call us at +27 64 6152989 or email vunosethu@gmail.com');
+    });
+});
+
